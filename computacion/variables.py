f37f6d9bb1ee62b8a8a91710c9e3ac3fe66e3b9c
Yamileth-klp <karimepinedo4@gmail.com>
2026-02-26 18:05:01 -0600

    practica7 karime

---
+++ computacion/variables.py
@@ -0,0 +1,9 @@
+# Ejemplo:
+nombre = "Karime"
+print(nombre, type(nombre))
+edad = "17"
+print (edad, type(edad))
+estatura = "1.67"
+print(estatura, type(estatura))
+es_estudiante = True
+print(es_estudiante, type(es_estudiante))ss
